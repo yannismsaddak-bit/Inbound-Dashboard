84fb123b2aa77311ed3f692dacfc566b7dc376fb
yannismsaddak-bit <yannis.msaddak@hellofresh.fr>
2026-09-04 13:09:24 +0000

    Add Montserrat font

--- inbound-dashboard/pages/_app.js
+++ inbound-dashboard/pages/_app.js
@@ -1,7 +1,12 @@
 import { SessionProvider } from 'next-auth/react'
+import Head from 'next/head'
+
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet" />
+      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   )
